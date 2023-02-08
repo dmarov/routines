2f2c0021ae8a3dfa07f5701b76477853e3cfc6f5
Dmitry Marov <d.marov94@gmail.com>
2023-02-08 13:46:47 +0300

    improved is worktime

--- is-worktime.sh
+++ is-worktime.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env zsh
 
 # script returns error if today is weekend
-# use like: ./is-worktime && mattermost
+# use like: ./is-worktime && mattermost-desktop
 
 if [[ $(date +%u) -gt 5 ]];
+then
+    exit 1;
+fi
+
+if [[ $(date +%H) -lt 8 ]];
+then
+    exit 1;
+fi
+
+if [[ $(date +%H) -gt 19 ]];
 then
     exit 1;
 fi
